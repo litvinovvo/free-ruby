18b66f24d45b69106d536452a5e1bd4b48292638
Vladimir Litvinov <litvinovvo@gmail.com>
2018-05-30 17:36:16 +0500

    Revert "Revert "add ruby tasks""
    This reverts commit a114996fa6989869b8597642a23a3311f8fb90e7.

---
+++ task2.rb
@@ -0,0 +1,39 @@
+#  Task 2
+
+class String
+    def is_i?
+       /\A[-+]?\d+\z/ === self
+    end
+end
+
+n = ARGV[0]
+
+
+if n.nil? || !n.is_i?
+    abort("please, enter integer number")
+end
+
+n = n.to_i
+
+if n.abs > 10000
+    abort("number should be in range from -10000 to 10000")
+end
+
+def fib(n) 
+    if n == 0 then return 0
+    elsif n == 1 || n == 2 || n== -1 then return 1
+    elsif n == -2 then return -1
+    end
+    a = 1;
+    b = 1;
+    nAbs = n.abs
+    (3..nAbs).each do |;c|
+        c = a+b
+        a = b
+        b = c
+    end
+    return n < 0 ? ((-1)**(n+1))*b : b    
+end
+
+puts fib(n)
+
