de4de1178833fbe8f291aa06a9ccff9b63c513da
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-01 18:59:49 +0500

    small fix task1.js

--- task1.js
+++ task1.js
@@ -2,6 +2,7 @@ const args = process.argv;
 const str = args.slice(2).join(" ");
 
 function checkPalindrome(str){
+    if(str < 1)return "NO";
     const strClean = str.replace(/[\W_]/gi, '').toLowerCase();
     const strReverse = strClean.split("").reverse().join("");
     return strClean === strReverse ? "YES" : "NO";
