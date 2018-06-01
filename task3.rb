0badb54f182882009bc7c6b4901f4f13a3606d9b
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-01 21:42:11 +0500

    only 1 task

--- task3.rb
+++ task3.rb
@@ -1,12 +1,12 @@
-str = ''
-for i in 0 ... ARGV.length
-    str = str + ARGV[i]
-end
+# str = ''
+# for i in 0 ... ARGV.length
+#     str = str + ARGV[i]
+# end
 
-res = []
+# res = []
 
-v = if str.scan(/[aeiou]/i).any? then res.push(str.scan(/[aeiou]/i).join("")) end
-c = if str.scan(/[bcdfghjklmnpqrstvwxyz]/i).any? then res.push(str.scan(/[bcdfghjklmnpqrstvwxyz]/).join("")) end
-d = if str.scan(/[\d]/).any? then res.push(str.scan(/[\d]/).join("")) end
+# v = if str.scan(/[aeiou]/i).any? then res.push(str.scan(/[aeiou]/i).join("")) end
+# c = if str.scan(/[bcdfghjklmnpqrstvwxyz]/i).any? then res.push(str.scan(/[bcdfghjklmnpqrstvwxyz]/).join("")) end
+# d = if str.scan(/[\d]/).any? then res.push(str.scan(/[\d]/).join("")) end
 
-if str.length > 0 then puts res.join(" ") end
+# if str.length > 0 then puts res.join(" ") end
