49d46df8d6f2d6c3e72b0a8be6d5a901a3d371ba
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-02 00:55:45 +0500

    change task1.rb

--- task1.rb
+++ task1.rb
@@ -4,4 +4,4 @@ def isPalindrome (s)
     s.downcase.scan(/\w/).eql?(s.downcase.scan(/\w/).reverse) ? 'YES' : 'NO'
 end
 
-if s.length > 0 then puts isPalindrome(s) end
+if s.to_s.length > 0 then puts isPalindrome(s) end
