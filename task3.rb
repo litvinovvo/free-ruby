52cf4fc0afff0c3845e61e0e23f9537e5db790fb
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-01 17:17:57 +0500

    update task3.rb

--- task3.rb
+++ task3.rb
@@ -1,13 +1,8 @@
 str = ARGV[0]
-
-v = str.scan(/[aeiou]/i)
-c = str.scan(/[bcdfghjklmnpqrstvwxyz]/i)
-d = str.scan(/[\d]/)
-
 res = []
 
-if(v.length > 0) then res.insert(-1,v.join) end
-if(c.length > 0) then res.insert(-1,c.join) end
-if(d.length > 0) then res.insert(-1,d.join) end
+v = if str.scan(/[aeiou]/i).any? then res.push(str.scan(/[aeiou]/i).join("")) end
+c = if str.scan(/[bcdfghjklmnpqrstvwxyz]/i).any? then res.push(str.scan(/[bcdfghjklmnpqrstvwxyz]/).join("")) end
+d = if str.scan(/[\d]/).any? then res.push(str.scan(/[\d]/).join("")) end
 
 puts res.join(" ")
