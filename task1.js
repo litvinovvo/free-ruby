f1afe078edb591ff9d76d08fd446e31952b6b93d
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-02 17:39:48 +0500

    comment task1.js

--- task1.js
+++ task1.js
@@ -1,10 +1,10 @@
-const args = process.argv;
-const str = args.slice(1).join(" ");
+// const args = process.argv;
+// const str = args.slice(1).join(" ");
 
-function isPalindrome(str){
-    const strClean = str.replace(/[\W_]/gi, '').toLowerCase();
-    const strReverse = strClean.split("").reverse().join("");
-    return strClean === strReverse ? "YES" : "NO";
-}
+// function isPalindrome(str){
+//     const strClean = str.replace(/[\W_]/gi, '').toLowerCase();
+//     const strReverse = strClean.split("").reverse().join("");
+//     return strClean === strReverse ? "YES" : "NO";
+// }
 
-if(str.length > 0)process.stdout.write(isPalindrome(str));
+// if(str.length > 0)process.stdout.write(isPalindrome(str));
