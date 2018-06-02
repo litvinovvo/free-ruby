1794c821476e7b542aaf1441e5014c3a9a7ba1f9
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-02 21:10:51 +0500

    grab all args task1.rb

--- task1.rb
+++ task1.rb
@@ -1,7 +1,10 @@
-s = ARGV[0]
+s = ''
+for i in 0 ... ARGV.length
+    s = s + ARGV[i]
+end
 
 def isPalindrome (s)
     s.downcase.gsub(/[\W_]/,'').eql?(s.downcase.gsub(/[\W_]/,'').reverse) ? 'YES' : 'NO'
 end
 
-if s.to_s.length > 0 then puts isPalindrome(s) end
+if s.length > 0 then puts isPalindrome(s) end
