939d0d9bc23f481e4b5c11d20b77593dae017232
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-02 21:21:54 +0500

    uncomment all

--- task2.js
+++ task2.js
@@ -1,17 +1,17 @@
-// const args = process.argv;
+const args = process.argv;
 
-// function fib(n) {
-//     if (Math.abs(n) < 2) return Math.abs(n);
+function fib(n) {
+    if (Math.abs(n) < 2) return Math.abs(n);
 
-//     let a = 0;
-//     let b = 1;
-//     const nAbs = Math.abs(n)
-//     for (let i = 2; i <= nAbs; i++) {
-//         let c = a + b;
-//         a = b;
-//         b = c;
-//     }
-//     return n < 0 ? b*(Math.pow(-1, n + 1)) : b;
-// }
+    let a = 0;
+    let b = 1;
+    const nAbs = Math.abs(n)
+    for (let i = 2; i <= nAbs; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return n < 0 ? b*(Math.pow(-1, n + 1)) : b;
+}
 
-// if (args[2]) process.stdout.write(fib(parseInt(args[2])).toString());
+if (args[2]) process.stdout.write(fib(parseInt(args[2])).toString());
