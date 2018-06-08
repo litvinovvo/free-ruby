111f6f778edd47a2dabca11837f5b58bbba1f427
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-08 16:22:30 +0500

    linted task2.rb

--- task2.rb
+++ task2.rb
@@ -1,13 +1,13 @@
-def fib(n) 
-    if (n.abs < 2 ) then return n.abs end
-    a = 0;
-    b = 1;
-    (2..n.abs).each do |;c|
-        c = a+b
-        a = b
-        b = c
-    end
-    return n < 0 ? ((-1)**(n+1))*b : b    
+def fib(fib_n)
+  fib_n.abs < 2 && (return fib_n.abs)
+  a = 0
+  b = 1
+  (2..fib_n.abs).each do
+    c = a + b
+    a = b
+    b = c
+  end
+  fib_n < 0 ? ((-1)**(fib_n + 1)) * b : b
 end
 
-if(!ARGV[0].nil?) then puts fib(ARGV[0].to_i) end
+!ARGV[0].nil? && (puts fib(ARGV[0].to_i).to_s)
