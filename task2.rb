32da2aba048269050eaf21fa70ee8974cb450780
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-08 16:26:28 +0500

    change indentation

--- task2.rb
+++ task2.rb
@@ -1,13 +1,13 @@
 def fib(fib_n)
-  fib_n.abs < 2 && (return fib_n.abs)
-  a = 0
-  b = 1
-  (2..fib_n.abs).each do
-    c = a + b
-    a = b
-    b = c
-  end
-  fib_n < 0 ? ((-1)**(fib_n + 1)) * b : b
+    fib_n.abs < 2 && (return fib_n.abs)
+    a = 0
+    b = 1
+    (2..fib_n.abs).each do
+        c = a + b
+        a = b
+        b = c
+    end
+    fib_n < 0 ? ((-1)**(fib_n + 1)) * b : b
 end
 
 !ARGV[0].nil? && (puts fib(ARGV[0].to_i).to_s)
