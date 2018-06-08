ab0e671f951dc53d30d1420f9fa0500e8a5e0a46
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-08 16:40:19 +0500

    rename and small optimization

--- task1.rb
+++ task1.rb
@@ -3,8 +3,8 @@ for i in 0 ... ARGV.length
     s = s + ARGV[i]
 end
 
-def isPalindrome (s)
+def is_palindrome(s)
     s.downcase.gsub(/[\W_]/,'').eql?(s.downcase.gsub(/[\W_]/,'').reverse) ? 'YES' : 'NO'
 end
 
-if s.length > 0 then puts isPalindrome(s) end
+if s.length > 0 then puts is_palindrome(s) end
--- task2.rb
+++ task2.rb
@@ -1,8 +1,9 @@
 def fib(fib_n)
-    fib_n.abs < 2 && (return fib_n.abs)
+    fib_n_abs = fib_n.abs
+    fib_n_abs < 2 && (return fib_n_abs)
     a = 0
     b = 1
-    (2..fib_n.abs).each do
+    (2..fib_n_abs).each do
         c = a + b
         a = b
         b = c
