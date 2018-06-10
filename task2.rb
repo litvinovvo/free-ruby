c03e11ba41bff84309a5bd1bb964202d74771ecc
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-10 05:12:47 +0500

    return 1 if not digit in task2

--- task2.rb
+++ task2.rb
@@ -1,4 +1,12 @@
+class String
+    def is_i?
+       /\A[-+]?\d+\z/ === self
+    end
+end
+
 def fib(fib_n)
+    !fib_n.is_i? && (return 1)
+    fib_n = fib_n.to_i
     fib_n_abs = fib_n.abs
     fib_n_abs < 2 && (return fib_n_abs)
     a = 0
@@ -11,4 +19,4 @@ def fib(fib_n)
     fib_n < 0 ? ((-1)**(fib_n + 1)) * b : b
 end
 
-!ARGV[0].nil? && (puts fib(ARGV[0].to_i).to_s)
+!ARGV[0].nil? && (puts fib(ARGV[0]))
