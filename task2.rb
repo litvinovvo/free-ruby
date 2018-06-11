ed05462e93e2452cabffbf265810f11c3a641849
Vladimir Litvinov <litvinovvo@gmail.com>
2018-06-11 14:44:41 +0500

    fast fib func

--- task2.rb
+++ task2.rb
@@ -1,22 +1,21 @@
-class String
-    def is_i?
-       /\A[-+]?\d+\z/ === self
+def fast_fib(fib_n)
+    fib_n < 0 ? ((-1)**(fib_n + 1)) * _fib(fib_n.abs)[0] : _fib(fib_n)[0]
+end
+  
+def _fib(n)
+    if n == 0
+        return [0, 1]
+    else
+        a, b = _fib(n / 2)
+        c = a * (b * 2 - a)
+        d = a * a + b * b
+
+        if n % 2 == 0
+            [c, d]
+        else
+            [d, c + d]
+        end
     end
 end
 
-def fib(fib_n)
-    !fib_n.is_i? && (return 1)
-    fib_n = fib_n.to_i
-    fib_n_abs = fib_n.abs
-    fib_n_abs < 2 && (return fib_n_abs)
-    a = 0
-    b = 1
-    (2..fib_n_abs).each do
-        c = a + b
-        a = b
-        b = c
-    end
-    fib_n < 0 ? ((-1)**(fib_n + 1)) * b : b
-end
-
-!ARGV[0].nil? && (puts fib(ARGV[0]))
+puts fast_fib(ARGV[0].to_i)
